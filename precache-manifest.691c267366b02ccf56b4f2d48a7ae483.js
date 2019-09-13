e525f23008d031016c72654014c436293f09a79e
EEYousef <eeeyyy@gmail.com>
2019-09-13 19:29:58 +0300

    Updates

---
+++ precache-manifest.691c267366b02ccf56b4f2d48a7ae483.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e5310a3035ef17484b1dd8f3a453b7a",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "7bf73d3d9c612d1f65ee",
+    "url": "/monster/static/css/main.94a1fea0.chunk.css"
+  },
+  {
+    "revision": "0792b5e2043d95efda44",
+    "url": "/monster/static/js/2.1159b1ef.chunk.js"
+  },
+  {
+    "revision": "7bf73d3d9c612d1f65ee",
+    "url": "/monster/static/js/main.b0b3cca0.chunk.js"
+  },
+  {
+    "revision": "f608bbb0e64f62745433",
+    "url": "/monster/static/js/runtime~main.1e736457.js"
+  }
+]);
